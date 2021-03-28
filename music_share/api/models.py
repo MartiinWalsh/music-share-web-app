06afd45ed8f3e5d188d55860049429adb8ed0152
m-codes <martinwalsh.dev@gmail.com>
2021-03-28 17:48:15 +0100

    Handling POST requests

--- music_share/api/models.py
+++ music_share/api/models.py
@@ -2,23 +2,26 @@ from django.db import models
 import string
 import random
 
+
 def generate_unique_room_code():
     length = 7
 
     while True:
         # Generate random code of predefined length, only containing uppercase ascii chars
-        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        code = "".join(random.choices(string.ascii_uppercase, k=length))
         # Check if code is unique across rooms
         if Room.objects.filter(code=code).count() == 0:
             break
 
     return code
 
+
 # Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length = 8, default="", unique=True)
+    code = models.CharField(
+        max_length=8, default=generate_unique_room_code, unique=True
+    )
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
-
--- music_share/api/serializers.py
+++ music_share/api/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from .models import Room
 
+# Serialize Room object to return as a response
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ('__all__')
+        fields = (
+            "id",
+            "code",
+            "host",
+            "guest_can_pause",
+            "votes_to_skip",
+            "created_at",
+        )
+
+
+# Serialize data from a request
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ("guest_can_pause", "votes_to_skip")
